3869408d4d3b1b16c79bb8c7943c80d41598b81b
andrey0902 <andreypavlov1706@gmail.com>
2018-08-10 14:40:33 +0300

    add firebase config in the invironment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBM-aX1BonA9hJlkDC0hCnpSslZhsMwhHU',
+    authDomain: 'oshop-c7d62.firebaseapp.com',
+    databaseURL: 'https://oshop-c7d62.firebaseio.com',
+    projectId: 'oshop-c7d62',
+    storageBucket: 'oshop-c7d62.appspot.com',
+    messagingSenderId: '362715120710'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBM-aX1BonA9hJlkDC0hCnpSslZhsMwhHU',
+    authDomain: 'oshop-c7d62.firebaseapp.com',
+    databaseURL: 'https://oshop-c7d62.firebaseio.com',
+    projectId: 'oshop-c7d62',
+    storageBucket: 'oshop-c7d62.appspot.com',
+    messagingSenderId: '362715120710'
+  }
 };
 
 /*
